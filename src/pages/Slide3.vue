8afc2fed0580db7af5d558d2930d993eb0dfa23a
luc41 <whthunder@live.cn>
2020-04-08 20:58:56 +0800

    add router

---
+++ src/pages/Slide3.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page>
+    <span class="text-h1">test3</span>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "Slide3"
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/Slide4.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page>
+    <span class="text-h1">test4</span>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "Slide4"
+};
+</script>
+
+<style>
+</style>
