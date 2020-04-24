7fb4f6a781ee1b3d754875f2d1c4d25e2f6a0cd5
luc41 <whthunder@live.cn>
2020-04-24 22:13:05 +0800

    add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex-center">
+    <q-img src="../assets/404.png"></q-img>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "Slide3",
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style>
+</style>
