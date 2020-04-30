d0366e9621f88addc17767b498973dd1c60e595a
luc41 <whthunder@live.cn>
2020-04-30 23:24:56 +0800

    fix: 404 page layout

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,33 +1,35 @@
 <template>
-  <q-page class="">
+  <q-page class>
     <div class="q-pa-md column">
       <transition name="slide" mode="out-in" appear>
-      <div class="col self-center" style="margin-top: 4em;">
-        <span class="text-h1 text-weight-bold non-selectable">ERROR:404!</span>
-      </div>
+        <div class="col self-center">
+          <span class="text-h1 text-weight-bold non-selectable">ERROR:404!</span>
+        </div>
       </transition>
       <transition name="slide" mode="out-in" appear>
-      <div class="col self-center" style="transition-delay: .2s;">
-        <span class="text-h4 non-selectable">您所访问的页面不存在</span>
-      </div>
+        <div class="col self-center" style="transition-delay: .2s;">
+          <span class="text-h4 non-selectable">您所访问的页面不存在</span>
+        </div>
       </transition>
       <transition name="slide" mode="out-in" appear>
-      <div class="col" style="transition-delay: .4s;">
-        <q-img id="img404" alt="404" :src="imgSrc" spinner-color="white" />
-      </div>
-      </transition>
-      <transition name="slide" mode="out-in" appear>
-      <div class="col" style="transition-delay: .6s;">
-        <q-btn
-          class="full-width"
-          icon="home"
-          label="返回首页"
-          size="xl"
-          to="/"
-          color="grey-14"
-          unelevated
-        />
-      </div>
+        <div class="col" style="transition-delay: .4s;">
+          <div class="row">
+            <div class="col-1" />
+            <div class="col">
+              <q-img id="img404" alt="404" :src="imgSrc" spinner-color="white" />
+              <q-btn
+                class="full-width"
+                icon="home"
+                label="返回首页"
+                size="xl"
+                to="/"
+                color="grey-14"
+                unelevated
+              />
+            </div>
+            <div class="col-1" />
+          </div>
+        </div>
       </transition>
     </div>
   </q-page>
@@ -38,7 +40,7 @@ export default {
   name: "page404",
   data() {
     return {
-      imgSrc: "https://s1.ax1x.com/2020/04/27/JRYZk9.md.png"
+      imgSrc: "https://s1.ax1x.com/2020/04/27/JRYZk9.png"
     };
   }
 };
@@ -46,7 +48,8 @@ export default {
 
 <style lang="sass" scoped>
 #img404 
-  height: 500px
+  height: 600px
+  position: relative
 .col
   margin: 1em 0 1em 0
 </style>
