a63b2509a9fdd4c28e53a6d0cd5c0957d3aae202
luc41 <whthunder@live.cn>
2020-05-07 18:16:08 +0800

    fix: bugs in page404

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -3,12 +3,12 @@
     <div class="q-pa-md column">
       <transition name="slide" mode="out-in" appear>
         <div class="col self-center">
-          <span id="title" class="text-h1 text-weight-bold non-selectable">ERROR：404!</span>
+          <span id="title" class="text-h2 text-weight-bold">ERROR:PAGE NOT FOUND</span>
         </div>
       </transition>
       <transition name="slide" mode="out-in" appear>
         <div class="col self-center" style="transition-delay: .2s;">
-          <span id="subtitle" class="text-h4 non-selectable">您访问的页面不存在，请检查URL后再试</span>
+          <span id="subtitle" class="text-h4">您访问的页面不存在，请检查URL后再试</span>
         </div>
       </transition>
       <transition name="slide" mode="out-in" appear>
